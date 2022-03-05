4d249f21c710321371776ba9bc1eea7efe48ff76
pablo <pablitoogpde@gmail.com>
2022-03-05 17:17:21 -0300

    Agregar archivos de proyecto.

---
+++ loguin1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace loguin1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpas = new System.Windows.Forms.TextBox();
+            this.btniniciar = new System.Windows.Forms.Button();
+            this.btncanselar = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("CentSchbkCyrill BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(182, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("CentSchbkCyrill BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(182, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "contraseña";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(363, 75);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(130, 20);
+            this.txtuser.TabIndex = 2;
+            // 
+            // txtpas
+            // 
+            this.txtpas.Location = new System.Drawing.Point(363, 122);
+            this.txtpas.Name = "txtpas";
+            this.txtpas.Size = new System.Drawing.Size(130, 20);
+            this.txtpas.TabIndex = 3;
+            // 
+            // btniniciar
+            // 
+            this.btniniciar.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btniniciar.Location = new System.Drawing.Point(186, 186);
+            this.btniniciar.Name = "btniniciar";
+            this.btniniciar.Size = new System.Drawing.Size(168, 36);
+            this.btniniciar.TabIndex = 4;
+            this.btniniciar.Text = "iniciar secion";
+            this.btniniciar.UseVisualStyleBackColor = true;
+            this.btniniciar.Click += new System.EventHandler(this.btniniciar_Click);
+            // 
+            // btncanselar
+            // 
+            this.btncanselar.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncanselar.Location = new System.Drawing.Point(382, 186);
+            this.btncanselar.Name = "btncanselar";
+            this.btncanselar.Size = new System.Drawing.Size(120, 36);
+            this.btncanselar.TabIndex = 5;
+            this.btncanselar.Text = "cancelar";
+            this.btncanselar.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btncanselar);
+            this.Controls.Add(this.btniniciar);
+            this.Controls.Add(this.txtpas);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpas;
+        private System.Windows.Forms.Button btniniciar;
+        private System.Windows.Forms.Button btncanselar;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
---
+++ loguin1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace loguin1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Click(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "uriel" && (txtpas.Text == "2727"))
+            { Form formulario = new Form2();
+                formulario.Show();
+            }
+            else
+            { MessageBox.Show("ERROR"); }
+        }
+        }
+    }
+
